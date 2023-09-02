4de4744446aa352346abcf2ee6b9f363fc0b50bf
Euphemia Zhang <euph.zhang@gmail.com>
2023-09-02 01:54:44 -0400

    add chart

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,30 +3,32 @@ import snowflake.connector
 import pandas
 import requests
 
-
 ### Snowflake connection
 sn_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
 sn_cur = sn_cnx.cursor()
-sn_cur.execute("SELECT COUNT(1) FROM RAW.CHICAGO_CRIMES;")
-data_row = sn_cur.fetchall()
 
+#total count
+sn_cur.execute("SELECT COUNT(1) FROM RAW.CHICAGO_CRIMES;")
+total_cnt = sn_cur.fetchall()
+
+#Group by 
+sn_cur.execute("select Primary_Type, count(1) from RAW.CHICAGO_CRIMES group by Primary_Type order by Primary_Type;")
+prim_type_grp = sn_cur.fetchall()
 
 ### Set the title/header
 st.set_page_config(layout='wide')
 st.header('Welcome to my streamlit/snowflake experiments.')
 st.subheader('Hope you enjoy my learning journey!')
 
-##
-
-
 ## Layout
 col1, col2, col3, col4 = st.columns((2,1,1,1))
 with col1:
-	st.write('first column')
-	st.write(f'there are {data_row} rows!')
-
+	st.write('Dataset general information')
+	st.write(f'there are {total_cnt[0]} rows!')
+	
 with col2:
-	st.write('2nd column')
+	st.write('Bar chart group by primary_type')
+	st.bar_char(prim_type_grp)
 	
 with col3:
 	st.write('3rd column')
