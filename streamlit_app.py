af86da2bd59645a36fd0ad58ed3f481e6530a795
Euphemia Zhang <euph.zhang@gmail.com>
2023-09-02 02:05:57 -0400

    rm barchar

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,7 @@ total_cnt = sn_cur.fetchall()
 #Group by 
 sn_cur.execute("select Primary_Type, count(1) from RAW.CHICAGO_CRIMES group by Primary_Type order by Primary_Type;")
 fetch_ptg = sn_cur.fetchall()
-prim_type_grp = streamlit.dataframe(fetch_ptg)
+prim_type_grp = st.dataframe(fetch_ptg)
 
 ### Set the title/header
 st.set_page_config(layout='wide')
@@ -29,7 +29,7 @@ with col1:
 	
 with col2:
 	st.write('Bar chart group by primary_type')
-	#st.bar_char(prim_type_grp)
+	st.bar_char(prim_type_grp)
 	
 with col3:
 	st.write('3rd column')
