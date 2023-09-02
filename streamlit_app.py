2246b6e3c8213bafb94bb07621133e55ea5dc225
Euphemia Zhang <euph.zhang@gmail.com>
2023-09-02 01:27:46 -0400

    added connection to snowflake

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,25 @@
 import streamlit as st
 
-st.set_page_config(layout='wide')
+### Snowflake connection
+sn_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+sn_cur = sn_cnx.cursor()
+sn_cur.execute("SELECT COUNT(1) FROM RAW.CHICAGO_CRIMES;")
+data_row = sn_cur.fetchall()
 
+
+### Set the title/header
+st.set_page_config(layout='wide')
 st.header('Welcome to my streamlit/snowflake experiments.')
 st.subheader('Hope you enjoy my learning journey!')
 
-col1, col2, col3, col4 = st.columns((2,1,1,1))
+##
 
+
+## Layout
+col1, col2, col3, col4 = st.columns((2,1,1,1))
 with col1:
 	st.write('first column')
+	#st.write(f'there are {data_row} rows!')
 
 with col2:
 	st.write('2nd column')
