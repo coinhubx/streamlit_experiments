633863684bcc1e74307e7cc3657f9566d83b97a9
Euphemia Zhang <euph.zhang@gmail.com>
2023-09-02 01:13:37 -0400

    changed

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,16 +5,16 @@ st.set_page_config(layout='wide')
 st.header('Welcome to my streamlit/snowflake experiments.')
 st.subheader('Hope you enjoy my learning journey!')
 
-col1, col2, col3, col4 = st.beta_columns((2,1,1,1))
+col1, col2, col3, col4 = st.columns((2,1,1,1))
 
 with col1:
-	st.line_chart('first column')
+	st.write('first column')
 
 with col2:
-	st.area_chart('2nd column')
+	st.write('2nd column')
 	
 with col3:
-	st.bar_chart('3rd column')
+	st.write('3rd column')
 	
 with col4:
-	st.altair_chart('4th column')
+	st.write('4th column')
