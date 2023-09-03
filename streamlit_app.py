ecb0479e4ff51a2b482bd9c9e1380ac841cb9716
Euphemia Zhang <euph.zhang@gmail.com>
2023-09-02 23:02:56 -0400

    ohno

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,9 +28,9 @@ with col1:
 with col2:
 	st.write('Bar chart group by primary_type')
 	sn_cur.execute("select Primary_Type, count(1) from RAW.CHICAGO_CRIMES group by Primary_Type order by Primary_Type;")
-	ptg = sn_cur.fetch_pandas_all()
-	st.write(ptg.columns)
-	st.dataframe(ptg)
+	#ptg = sn_cur.fetch_pandas_all()
+	#st.write(ptg.columns)
+	#st.dataframe(ptg)
 
 	
 with col3:
