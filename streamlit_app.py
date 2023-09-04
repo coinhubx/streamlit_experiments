345f95cabbe202f316a37587f32aba859c8c1932
Euphemia Zhang <euph.zhang@gmail.com>
2023-09-04 06:21:14 -0400

    m view and buttons

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,9 +7,9 @@ import requests
 sn_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
 sn_cur = sn_cnx.cursor()
 
-#total count
-sn_cur.execute("SELECT COUNT(1) FROM RAW.CHICAGO_CRIMES;")
-total_cnt = sn_cur.fetchall()
+#read the materialized view
+sn_cur.execute("select * FROM RAW.SUMMARY_CRIME_COUNTS;")
+ptg_pd = sn_cur.fetch_pandas_all()
 
 #Group by 
 
@@ -23,17 +23,31 @@ st.subheader('Hope you enjoy my learning journey!')
 col1, col2, col3, col4 = st.columns((1,2,1,1))
 with col1:
 	st.write('Dataset general information')
-	st.write(f'there are {total_cnt[0][0]} rows in the table!')
+	st.write(f'there are {ptg_pd.shape} rows in the table!')
 	
 with col2:
 	st.write('Bar chart group by primary_type')
-	sn_cur.execute("select * FROM RAW.SUMMARY_CRIME_COUNTS;")
-	ptg = sn_cur.fetchall()
-	#st.write(ptg.columns)
-	ptg_ds = pd.DataFrame(ptg)
-	##ptg_ds = ptg_ds.set_index("PRIMARY_TYPE")
-	st.dataframe(ptg_ds.columns)
 
+	#st.write(ptg.columns)
+	#ptg_ds = pd.DataFrame(ptg, columns=sn_cur.description)
+	#ptg_ds = ptg_ds.set_index("PRIMARY_TYPE")
+	#st.dataframe(ptg_ds)
+	
+	st.write('------------')
+	#st.write('st.text_input demo - max_chars sets max characters allowed. if exceed, copy/paste disabled')
+	#text = st.text_input('Insert the primary type.', value = 'ROBBERY', max_chars = 30)
+	
+	#st.write('st.button demo - 2 buttons')
+	#st.button("Reset", type="primary")
+	#if st.button('Submit'):
+
+	#	st.dataframe(ptg)
+	
+	st.write('------------')
+	#st.write('st.multiselect : select from the list provided.')
+	#sn_cur.execute(f"select distinct PRIMARY_TYPE FROM RAW.SUMMARY_CRIME_COUNTS;")
+	#ptg_list = sn_cur.fetchall()
+	#options = st.multiselect('Select the primary type(s)', list(map(lambda x: x[0], ptg_list)))
 	
 	
 	
