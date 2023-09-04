fa3cee5cf21b597c9cb9bd55eb2b3ccbf69b4036
Euphemia Zhang <euph.zhang@gmail.com>
2023-09-04 04:58:15 -0400

    m view and buttons

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,15 +23,23 @@ st.subheader('Hope you enjoy my learning journey!')
 col1, col2, col3, col4 = st.columns((1,2,1,1))
 with col1:
 	st.write('Dataset general information')
-	st.write(f'there are {total_cnt[0]} rows!')
+	st.write(f'there are {total_cnt[0][0][0]} rows in the table!')
 	
 with col2:
 	st.write('Bar chart group by primary_type')
-	sn_cur.execute("select Primary_Type, count(1) from RAW.CHICAGO_CRIMES group by Primary_Type order by Primary_Type;")
+	sn_cur.execute("select * FROM RAW.SUMMARY_CRIME_COUNTS;")
 	ptg = sn_cur.fetchall()
-	st.write(sn_cur.describe())
 	#st.write(ptg.columns)
 	st.dataframe(ptg)
+	
+	number = st.number_input('Insert the primary type.')
+	st.write('The current number is ', number)
+	
+	st.button("Submit", type="primary")
+	if st.button('Say hello'):
+		st.write('Why hello there')
+	else:
+		st.write('Goodbye')
 
 	
 with col3:
