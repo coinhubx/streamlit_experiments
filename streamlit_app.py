4818862870354bb994b0630caabdeea81cafc0f6
Euphemia Zhang <euph.zhang@gmail.com>
2023-09-04 22:17:04 -0400

    fixed

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,7 +43,7 @@ with col2:
 	st.write('------------')
 	st.write('st.multiselect : select from the list provided.')
 	options = st.multiselect('Select the primary type(s)', ptg_pd.index)
-	
+	st.write(options)
 
 	
 with col3:
