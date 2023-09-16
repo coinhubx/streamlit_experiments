267fcf521464dfa980e8ea4a8d659faf76b93452
Euphemia Zhang <euph.zhang@gmail.com>
2023-09-15 23:09:49 -0400

    chg

--- streamlit_app.py
+++ streamlit_app.py
@@ -68,7 +68,7 @@ with col3:
 	'bar chart'
 	# pick color for the bar
 	color_picked = st.color_picker('pick a color for the bar chart', '#f774d8')
-	ptg_pd[['PRIMARY_TYPE', 'COUNT']]
+	ptg_pd['PRIMARY_TYPE']
 	##st.bar_chart(ptg_pd_1, color = color_picked)
 	
 st.divider()
